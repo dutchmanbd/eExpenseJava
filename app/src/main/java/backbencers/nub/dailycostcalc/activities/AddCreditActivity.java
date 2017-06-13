61e58855cfc9f1406cdf6e1a4249c3a534b8b31a
Monir Hossain <mknishad1@ymail.com>
2017-06-13 15:10:35 +0600

    Add onClick listener for calendar button

--- app/src/main/java/backbencers/nub/dailycostcalc/activities/AddCreditActivity.java
+++ app/src/main/java/backbencers/nub/dailycostcalc/activities/AddCreditActivity.java
@@ -1,22 +1,28 @@
 package backbencers.nub.dailycostcalc.activities;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.AutoCompleteTextView;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import backbencers.nub.dailycostcalc.R;
 
 public class AddCreditActivity extends AppCompatActivity {
 
-    private EditText etDate;
+    private static EditText etDate;
     private ImageButton ibCalendar;
     private AutoCompleteTextView actvCategory;
     private EditText etDescription;
@@ -30,7 +36,15 @@ public class AddCreditActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         initializeViews();
-        setDate();
+        setInitialDate();
+
+        ibCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new DatePickerFragment();
+                newFragment.show(getSupportFragmentManager(), "datePicker");
+            }
+        });
     }
 
     private void initializeViews() {
@@ -41,7 +55,7 @@ public class AddCreditActivity extends AppCompatActivity {
         etAmount = (EditText) findViewById(R.id.edit_text_amount);
     }
 
-    private void setDate() {
+    private void setInitialDate() {
         String date = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
         etDate.setText(date);
     }
@@ -62,5 +76,31 @@ public class AddCreditActivity extends AppCompatActivity {
             case R.id.action_save_credit:
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            month += 1;
+            String finalDay = day > 9 ? (""+day) : ("0"+day);
+            String finalMonth = month > 9 ? (""+month) : ("0"+month);
+            String finalYear = ""+year;
+            etDate.setText("");
+            etDate.setText(finalDay + "-" + finalMonth + "-" + finalYear);
+        }
     }
 }
